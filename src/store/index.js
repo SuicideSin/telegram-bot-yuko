bc069042bc20111c356c1bd7643efd3c3433726b
Plusb Preco <plusb21@gmail.com>
2017-02-06 11:24:04 +0900

    Add missing file

---
+++ src/store/index.js
@@ -0,0 +1,30 @@
+import storeConfig from '../config/store';
+import StoreBase from './StoreBase';
+
+const store = new StoreBase();
+
+function getStore(name) {
+  return store.get(name);
+}
+
+function initStore() {
+  const storePromises = storeConfig.stores
+    .map(({name, options, indexingRules}) => createStore(name, options, indexingRules));
+
+  return Promise.all(storePromises);
+}
+
+async function createStore(name, options, indexingRules) {
+  const createdStore = await store.create(name, options);
+
+  if (Array.isArray(indexingRules)) {
+    await Promise.all(indexingRules.map((opts) => createdStore.ensureIndex(opts)));
+  }
+
+  return createdStore;
+}
+
+export {
+  getStore,
+  initStore,
+};
