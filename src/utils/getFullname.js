bd2d71ad4559d649872b776383d78197a236c214
Plusb Preco <plusb21@gmail.com>
2017-02-06 11:27:17 +0900

    Prefer template string over concat strings

--- src/utils/getFullname.js
+++ src/utils/getFullname.js
@@ -1,5 +1,5 @@
 function getFullname({first_name: firstName = '', last_name: lastName}) {
-  return firstName + (lastName ? ` ${lastName}` : '');
+  return `${firstName}${lastName ? ` ${lastName}` : ''}`;
 }
 
 export {
