b3d0edcf1861378a43b880310fed08b7d35374d3
Plusb Preco <plusb21@gmail.com>
2017-02-18 19:59:03 +0900

    Refactor `secured` decorator

--- src/decorators/secured.js
+++ src/decorators/secured.js
@@ -2,32 +2,24 @@ import HandlerError from '../core/HandlerError';
 import {verifySession} from '../store/actions/session';
 import authStrings from '../strings/auth';
 
-function secured(mapStore) {
-  const ensureLogin = async (username, errorOpts) => {
-    const store = await mapStore();
-    const hasSigned = await verifySession(store, username);
+function secured(mapStore, shouldBeEvent = false) {
+  return (target, key, descriptor) => {
+    const {value: func} = descriptor;
 
-    if (!hasSigned) {
-      throw new HandlerError(authStrings.notSigned, errorOpts);
-    }
-  };
+    return {
+      ...descriptor,
+      async value(...args) {
+        const [, {from: {username}}] = args;
+        const store = await mapStore();
+        const hasSigned = await verifySession(store, username);
 
-  return (Handler) => class SecuredHandler extends Handler {
-    didReceiveCommand(...args) {
-      const [, {from: {username}}] = args;
+        if (!hasSigned) {
+          throw new HandlerError(authStrings.notSigned, {event: shouldBeEvent});
+        }
 
-      // FIXME: Babel의 이슈로 인해 임시 조치
-      return ensureLogin(username)
-        .then(() => super.didReceiveCommand(...args));
-    }
-
-    didReceiveEvent(...args) {
-      const [, {from: {username}}] = args;
-
-      // FIXME: Babel의 이슈로 인해 임시 조치
-      return ensureLogin(username, {event: true})
-        .then(() => super.didReceiveEvent(...args));
-    }
+        return func.apply(this, args);
+      },
+    };
   };
 }
 
