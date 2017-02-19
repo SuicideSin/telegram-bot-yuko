4c6e2bbd5a1dca57c5fccf414ce800d246893087
Plusb Preco <plusb21@gmail.com>
2017-02-19 19:02:07 +0900

    Remove comment

--- src/handlers/auth/Logout.js
+++ src/handlers/auth/Logout.js
@@ -7,8 +7,6 @@ import getFullname from '../../utils/getFullname';
 import authStrings from '../../strings/auth';
 import commonsStrings from '../../strings/commons';
 
-// 하나로 정리 - 아예 시트 로직과 같이 만들고 그냥 데코레이터는 해당 함수를 받아서 처리하도록 하자
-
 class Logout extends Handler {
   getCommandTarget() {
     return createCommand(['logout', '로그아웃']);
