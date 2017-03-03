897b2602f18dc0a1c695270b4530ed85483b41a0
Plusb Preco <plusb21@gmail.com>
2017-03-03 12:49:43 +0900

    Adjust log level

--- src/globals/logger.js
+++ src/globals/logger.js
@@ -8,9 +8,9 @@ global.logger = new Logger({
   levels: {
     error: 0,
     warn: 1,
-    status: 2,
-    command: 3,
-    event: 4,
+    command: 2,
+    event: 3,
+    status: 4,
     info: 5,
   },
   transports: [
@@ -27,8 +27,8 @@ global.logger = new Logger({
 winston.addColors({
   error: 'red',
   warn: 'yellow',
-  status: 'blue',
   command: 'cyan',
   event: 'magenta',
+  status: 'blue',
   info: 'green',
 });
